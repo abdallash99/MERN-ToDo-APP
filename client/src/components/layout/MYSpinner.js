217a192e39a9bf4549630fc282cd3100b15ab9e7
abdallash <abd0173435@gmail.com>
2020-06-21 09:11:51 +0300

    adding spinner

---
+++ client/src/components/layout/MYSpinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spinner from '@bit/nexxtway.react-rainbow.spinner';
+
+export default function () {
+    return (
+        <div style={{ width: 300, height: 200 }}>
+            <Spinner variant="brand" size="large" />
+        </div>
+    )
+} 
--- client/src/components/page/Home.js
+++ client/src/components/page/Home.js
@@ -2,18 +2,20 @@ import React, { useEffect, useContext } from 'react'
 import TodoForm from '../todo/TodoForm'
 import TodoItems from '../todo/TodoItems'
 import AuthContext from './../../context/auth/authContext';
+import MYSpinner from '../layout/MYSpinner';
 const Home = () => {
     const authContext = useContext(AuthContext);
-    const { loadUser } = authContext;
+    const { loadUser, loading } = authContext;
     useEffect(() => {
         loadUser()
         // eslint-disable-next-line
     }, [])
     return (
-        <div className='container'>
-            <TodoForm />
-            <TodoItems />
-        </div>
+        loading ? <MYSpinner /> :
+            <div className='container'>
+                <TodoForm />
+                <TodoItems />
+            </div>
     )
 }
 
